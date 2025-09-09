f0a4e3c7d3c5fc8cdb2f1180758b5c7ccfcb55ad
Jack Moscrop <700002741@GoTVDSB.ca>
2025-09-09 09:24:53 -0400

    Topic 3 Assignment

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace Topic_3_Assignment
         {
             // Constants
             const double Pi = float.Pi;
+            const double sideA = 1;
+            const double sideB = 8;
             // Ints
             int graduationYear;
             int roomNumber;
@@ -17,6 +19,7 @@ namespace Topic_3_Assignment
             double circleArea;
             double circleRadius;
             double price;
+            double hypotenuseLength;
             // Strings
             string className;
             string myName;
@@ -27,15 +30,34 @@ namespace Topic_3_Assignment
             className = "Computer Science";
             myName = "Jack Moscrop";
             graduationYear = 2027;
+            // Area of a circle
             circleArea = (Pi * (circleRadius * circleRadius));
+            // Pythagorean Theorum
+            hypotenuseLength = ((sideA * sideA) + (sideB * sideB));
+            hypotenuseLength = double.Sqrt(hypotenuseLength);
+            
             // Writing The Variables
+            // Part 1
             Console.WriteLine($"This is room #{roomNumber}");
             Console.WriteLine($"The price is ${price}");
             Console.WriteLine($"This class is {className}");
             Console.WriteLine("");
+            // Part 2
+            Console.WriteLine("Name and graduation year");
             Console.WriteLine($"My name is {myName} and I will graduate in {graduationYear}.");
             Console.WriteLine("");
-            Console.WriteLine($"The area of a cicle with a radius of {circleRadius} will be {float.Round(circleArea, 2)} when rounded to 2 decimal places.");
+            // Part 3
+            Console.WriteLine("Area of a circle");
+            Console.WriteLine($"The area of a cicle with a radius of " + circleRadius + " will be " + double.Round(circleArea, 2) + " when rounded to 2 decimal places.");
+            Console.WriteLine("");
+            // Part 4
+            Console.WriteLine("Pythagorean Theorum");
+            Console.WriteLine("If a triangle with the side lengths of:");
+            Console.WriteLine($"Side A: {sideA}");
+            Console.WriteLine($"Side B: {sideB}");
+            Console.WriteLine("Will have a hypotenuse length of " + double.Round(hypotenuseLength, 2) + " when rounded to 2 decimal places.");
+
+            
             
 
 
